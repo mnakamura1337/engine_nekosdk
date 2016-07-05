5c428b020c0328cb7d9025418d367ee20942403f
Masumi Nakamura <mnakamura1337@yahoo.co.jp>
2016-07-05 18:15:35 +0900

    Implemented a few basic opcodes: dialogues, start/stop music, load bg

--- nekosdk2storyconverter.rb
+++ nekosdk2storyconverter.rb
@@ -1,9 +1,11 @@
+# coding: utf-8
 require_relative 'nekosdk_advscript2'
 
 require_relative 'converter_common'
 
 class Nekosdk2StoryConverter
   include ImplicitChars
+  include DetectQuotes
 
   def initialize(fn, meta, lang)
     @meta = meta
@@ -40,13 +42,108 @@ class Nekosdk2StoryConverter
   end
 
   def process_node(n)
-    puts "#{n.id}. type1=#{n.type1} type2=#{n.type2} some_ofs=#{n.some_ofs} next_id=#{n.next_id}"
+    case n.opcode
+    when 5
+      process_text_display(n)
+    when 10 # [背景ロード]
+      process_bg_load(n)
+    when 30 # [ＢＧＭ再生]
+      process_bgm_start(n)
+    when 31 # [ＢＧＭ停止]
+      process_bgm_stop(n)
+    else
+      puts "#{n.id}. type1=#{n.type1} opcode=#{n.opcode} some_ofs=#{n.some_ofs} next_id=#{n.next_id}"
+      n.strs.each_with_index { |s, i|
+        ss = s.value.rstrip
+        puts "  s#{i}: #{ss.encode('UTF-8')}" unless ss.empty?
+      }
+      puts
+    end
+  end
 
-    n.strs.each_with_index { |s, i|
-      ss = s.value.rstrip
-      puts "  s#{i}: #{ss.encode('UTF-8')}" unless ss.empty?
+  def process_text_display(n)
+    s = strs(n)
+    #cmd = s[0]
+    char = s[1]
+    txt = s[2]
+    voice_fn = s[3].gsub(/\\/, '/')
+
+    h = {
+      'txt' => {@lang => txt},
     }
 
-    puts
+    if char.empty?
+      h['op'] = 'narrate'
+    else
+      op, msg = detect_quotes(txt, true)
+      h['op'] = op
+      h['txt'] = {@lang => msg}
+      h['char'] = get_char_by_name(char)
+    end
+    h['voice'] = voice_fn unless voice_fn.empty?
+
+    @out << h
+
+    @out << {'op' => 'keypress'}
+  end
+
+  # [ＢＧＭ再生] bgm\M01-m.ogg
+  # ch:0/vol:40/pos:0/tm:3500
+  def process_bgm_start(n)
+    s = strs(n)
+    params = parse_cmd(s[0], '[ＢＧＭ再生]')
+
+    # TODO: vol, pos, tm
+    @out << {
+      'op' => 'sound_play',
+      'fn' => convert_fn(s[1]),
+      'channel' => "music#{params['ch']}",
+    }
+  end
+
+  # [ＢＧＭ停止]
+  # ch:1 / tm:2500
+  def process_bgm_stop(n)
+    s = strs(n)
+    params = parse_cmd(s[0], '[ＢＧＭ停止]')
+
+    # TODO: support "tm"
+    @out << {
+      'op' => 'sound_stop',
+      'channel' => "music#{params['ch']}",
+    }
+  end
+
+  # [背景ロード]
+  def process_bg_load(n)
+    s = strs(n)
+    @out << {
+      'op' => 'img',
+      'layer' => 'bg',
+      'fn' => convert_fn(s[1])
+    }
+  end
+
+  def strs(n)
+    n.strs.map { |s| s.value.rstrip.encode('UTF-8') }
+  end
+
+  def parse_cmd(cmd, expect_keyword)
+    lines = cmd.split(/\n/)
+    raise "invalid line 0: #{lines[0].inspect}" unless lines[0][0, expect_keyword.length] == expect_keyword
+
+    r = {}
+    lines[1..-1].each { |line|
+      line.split(/\//).each { |kv_pair|
+        k, v = kv_pair.strip.split(/:/)
+        r[k] = v
+      }
+    }
+
+    return r
+  end
+
+  def convert_fn(fn)
+    fn.gsub(/\\/, '/').downcase.gsub(/\.bmp$/, '.png')
   end
 end
